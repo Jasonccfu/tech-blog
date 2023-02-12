74b9296284a109ee653273543ea411bd5dfb2bb6
Jason Chen <jason.ccfu@gmail.com>
2023-02-12 07:29:19 -0800

    updated config

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,12 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  // process.env.DB_NAME,
+  "tech_db",
+  // process.env.DB_USER,
+  "root",
+  // process.env.DB_PASSWORD,
+  "",
   {
     host: "localhost",
     dialect: "mysql",
